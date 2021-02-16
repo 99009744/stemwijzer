2c079921919cb89bdd62cda04bd3432f26b79865
99009744 <55435447+99009744@users.noreply.github.com>
2021-02-16 15:53:39 +0100

    Vragen vertonen
    Vragen tonen op het scherm.
    Antwoorden opslaan in een array.
    Van 2 html documenten naar 1 gegaan.

--- js.js
+++ js.js
@@ -1,13 +1,46 @@
-let entries = Object.entries(parties);
-var container = document.getElementById("container");
+const start_screen = document.getElementById("start_container");
+const questions_screen = document.getElementById("questions_container");
+const title_question = document.getElementById("title_questions");
+const statement_questions = document.getElementById("statement_questions");
+const backbutton = document.getElementById("vorige");
+var answer = [];
+// Gonna help me switch between the questions.
+var counter = 0;
 function start(){
-   var elements = document.getElementsByClassName("startSchermContent");
-    while(elements.length > 0){
-        elements[0].parentNode.removeChild(elements[0]);
+    start_screen.style.display = "none";
+    questions_screen.style.display = "block";
+    console.log(counter);
+    next_Question();
+}
+
+function next_Question(){
+    title_questions.innerHTML = subjects[counter]['title'];
+    statement_questions.innerHTML = subjects[counter]['statement'];
+    if( counter == 0 ){
+        backbutton.style.display = "none";
     }
-    var questionBox = document.createElement("div");
-    questionBox.id = "questionBox";
-    questionBox.appendChild(container);
-    var title = document.createElement("h1");
-    title.innerHTML(entries[0][1]["name"]); 
+    else {
+        backbutton.style.display = "inline";
+    }
+}
+// Function to go 1 question back
+function back(){
+    counter --;
+    answer.pop();
+    title_questions.innerHTML = subjects[counter]['title'];
+    statement_questions.innerHTML = subjects[counter]['statement'];
+    if( counter == 0 ){
+        backbutton.style.display = "none";
+    }
+    else {
+        backbutton.style.display = "inline-block";
+    }
+}
+
+function buttonAnswer(button_Value){
+    console.log(button_Value.value);
+    answer.push({statement:button_Value.value});
+    console.log(answer);
+    counter++;
+    next_Question();
 }
