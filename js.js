e6d966e1f831f160a2ac6fc657583ce629bf2cf8
99009744 <55435447+99009744@users.noreply.github.com>
2021-03-02 19:28:38 +0100

    Antwoorden vergelijken en in % weergeven
    - antwoorden in een array stoppen = onnodig, punten systeem maakt het makkelijk.
    - punten naar % gemaakt & gesorteerd van hoog naar laag
    - buttons hover gefixt

--- js.js
+++ js.js
@@ -1,12 +1,14 @@
 const start_screen = document.getElementById("start_container");
 const questions_screen = document.getElementById("questions_container");
+const results_screen = document.getElementById("results_container");
 const title_question = document.getElementById("title_questions");
 const statement_questions = document.getElementById("statement_questions");
 const backbutton = document.getElementById("vorige");
-// Saves the answers in this arrray
-var answer = [];
-// Gonna help me switch between the questions.
-var counter = 0;
+const result_info = document.getElementById("results");
+var answer = []; // Saves the answers in this arrray.
+var partiesPoints = []; // Makes an array with objects with name and points of every party member.
+var counter = 0; // Gonna help me switch between the questions.
+
 function start(){
     start_screen.style.display = "none";
     questions_screen.style.display = "block";
@@ -33,8 +35,7 @@ function buttons(button_Value){
             item.style.backgroundColor = 'black'; 
             }
         document.getElementById(answer[counter]["statement"]).style.backgroundColor = "#01B4DC";
-        // removes the last object from the array
-        answer.pop();
+        answer.pop(); // removes the last object from the array
         title_questions.innerHTML = subjects[counter]['title'];
         statement_questions.innerHTML = subjects[counter]['statement'];
         if( counter == 0 ){
@@ -45,8 +46,7 @@ function buttons(button_Value){
         }
     }
     else{
-        // puts the value behind in the array
-        answer.push({statement:button_Value.value});
+        answer.push({statement:button_Value.value}); // puts the value behind in the array
         counter++;
         var buttonsColor = document.getElementsByClassName("nextButtons");
         for(var i = 0; i < buttonsColor.length; i++){
@@ -61,49 +61,45 @@ function buttons(button_Value){
         }
     }
 }
-function colorChange(){
-    var buttonsColor = document.getElementsByClassName("nextButtons");
-    for (let item of limbs) {
-        item.style.backgroundColor = 'black'; 
-    }
+
+function getPartyPoints(party){
+    var points = 0;
+     for(j=0; j<subjects.length; j++){
+         for(i=0; i<subjects[j].parties.length; i++){
+             if (answer[j].statement == subjects[j].parties[i].position){
+                 if(party == subjects[j].parties[i].name){
+                    points++;
+                 }
+                 
+             }
+         }
+     }
+     var returnPoints = points;
+     points = 0;
+     return returnPoints;
 }
 
-// checks the statements the party gave on the questions
-function getStatementsByPartyName(party){
-    var uitslag = [];
-    for(j=0; j<subjects.length; j++){ // loops questions
-        for(i=0; i<subjects[j].parties.length; i++){ // loop parties
-            if (party == parties[i]["name"]){
-                uitslag.push(subjects[j]["parties"][i]["position"]);
-            }
-        }
-    }
-    return uitslag;
-}
-// gets the party names by the value from checkResults
-function getPartyName(value){
-    var party = [];
-    for(i=value; i<=value; i++){
-        party.push(parties[i]["name"]);
-    }
-    return party;
-}
 // Makes a loop for each party in parties.
-// Calls getPartyName and getStatementsByPartyName to get their info.
 // Puts the info in an array
 function checkResults(){
-    var partiesAndStatements = [];
-    var i = 0;
-    parties.forEach(element => { // could have used element as var party but it made objects instead of arrays.
-        var party = getPartyName(i);
-        var uitslag = getStatementsByPartyName(party);
-        partiesAndStatements.push(party);
-        party.push(uitslag); 
-        i++;
+    parties.forEach(party => { 
+        var points = getPartyPoints(party.name);
+        partiesPoints.push({
+            name: party.name,
+            points: points});
     });
-    console.log(partiesAndStatements);
-}
-
-function comparingResults(){
-    
+    var sortByPoints = partiesPoints.slice(0);
+    sortByPoints.sort(function (a, b){
+        return  b.points - a.points;
+    });
+    questions_screen.style.display = "none";
+    results_screen.style.display = "block";
+    for(i=0; i<parties.length; i++){
+        var partiePercentages = document.createElement("p");
+        partiePercentages.setAttribute("id", sortByPoints[i].name)
+        partiePercentages.setAttribute("class", "parties");
+        var calculate = 100/30*sortByPoints[i].points;
+        partiePercentages.innerText = sortByPoints[i].name + " = " + calculate.toFixed(0) + "%";
+        result_info.appendChild(partiePercentages);
+    }
 }
