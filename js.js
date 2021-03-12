18052dbc13a42c9e52af99436205bfd53399902a
99009744 <55435447+99009744@users.noreply.github.com>
2021-03-12 19:50:06 +0100

    Hide parties button.
    3 buttons,
    1 to show parties with 10+ ppl in it
    1 that only show secular parties.
    1 to show all parties
    
    made the calculate var dynamic

--- js.js
+++ js.js
@@ -4,7 +4,7 @@ const results_screen = document.getElementById("results_container");
 const title_question = document.getElementById("title_questions");
 const statement_questions = document.getElementById("statement_questions");
 const backbutton = document.getElementById("vorige");
-const result_info = document.getElementById("results");
+const result_info = document.getElementById("bottom");
 var answer = []; // Saves the answers in this arrray.
 var partiesPoints = []; // Makes an array with objects with name and points of every party member.
 var counter = 0; // Gonna help me switch between the questions.
@@ -13,8 +13,8 @@ function start(){
     start_screen.style.display = "none";
     questions_screen.style.display = "block";
     next_Question();
+    
 }
-
 function next_Question(){
     title_questions.innerHTML = subjects[counter]['title'];
     statement_questions.innerHTML = subjects[counter]['statement'];
@@ -45,6 +45,29 @@ function buttons(button_Value){
             backbutton.style.display = "inline-block";
         }
     }
+    else if (button_Value.value == "big"){ // makes parties that are smaller then 10 not visible
+        partiesPoints.forEach(party => {
+            if(party.size < 10){
+                var small_party = document.getElementById(party.name);
+                small_party.style.display = "none";
+            }
+        });
+    }
+    else if (button_Value.value == "all"){
+        partiesPoints.forEach(party => {
+                var all_party = document.getElementById(party.name);
+                all_party.style.display = "inline-block";
+            });
+    }
+    else if(button_Value.value == "secular"){
+        partiesPoints.forEach(party => {
+            if(party.secular == false){
+                var secular_party = document.getElementById(party.name);
+                secular_party.style.display = "none";
+            }
+            
+        });
+    }
     else{
         answer.push({statement:button_Value.value}); // puts the value behind in the array
         counter++;
@@ -62,6 +85,7 @@ function buttons(button_Value){
     }
 }
 
+// Function to make a point system, if a party has the same position on a statement they gets a point
 function getPartyPoints(party){
     var points = 0;
      for(j=0; j<subjects.length; j++){
@@ -86,20 +110,24 @@ function checkResults(){
         var points = getPartyPoints(party.name);
         partiesPoints.push({
             name: party.name,
-            points: points});
+            points: points,
+            size: party.size,
+            secular: party.secular});
     });
-    var sortByPoints = partiesPoints.slice(0);
-    sortByPoints.sort(function (a, b){
-        return  b.points - a.points;
+    sortByPoints = partiesPoints.slice(0);
+    sortByPoints.sort(function (a, b){ // Sorts the points
+        return  b.points - a.points;    // Return the sorted points backwards so highest gets first.
     });
     questions_screen.style.display = "none";
     results_screen.style.display = "block";
     for(i=0; i<parties.length; i++){
         var partiePercentages = document.createElement("p");
-        partiePercentages.setAttribute("id", sortByPoints[i].name)
+        partiePercentages.setAttribute("id", sortByPoints[i].name);
         partiePercentages.setAttribute("class", "parties");
-        var calculate = 100/30*sortByPoints[i].points;
+        partiePercentages.style.display = "inline-block";
+        var calculate = 100/subjects.length*sortByPoints[i].points;
         partiePercentages.innerText = sortByPoints[i].name + " = " + calculate.toFixed(0) + "%";
         result_info.appendChild(partiePercentages);
     }
+
 }
