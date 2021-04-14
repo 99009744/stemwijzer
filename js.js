f0d73002a39b8ca7321ab780f7d4b54a6fb0d266
99009744 <55435447+99009744@users.noreply.github.com>
2021-04-14 16:03:36 +0200

    created extra weight for question
    created extra weight question inputs.
    created extra weight question calculator.

--- js.js
+++ js.js
@@ -1,4 +1,5 @@
 const start_screen = document.getElementById("start_container");
+const inportant_screen = document.getElementById("inportant_container");
 const questions_screen = document.getElementById("questions_container");
 const results_screen = document.getElementById("results_container");
 const title_question = document.getElementById("title_questions");
@@ -8,13 +9,56 @@ const result_info = document.getElementById("bottom");
 var answer = []; // Saves the answers in this arrray.
 var partiesPoints = []; // Makes an array with objects with name and points of every party member.
 var counter = 0; // Gonna help me switch between the questions.
+var selected_questions = [];
 
 function start(){
     start_screen.style.display = "none";
-    questions_screen.style.display = "block";
-    next_Question();
+    inportant_screen.style.display = "block";
+    
+    inportant_form();
     
 }
+function inportant_form(){
+    var inportant_container = document.getElementById("inportant_container");
+    subjects.forEach(subject => {  
+        var checkbox = document.createElement('input'); 
+        checkbox.type = "checkbox"; 
+        checkbox.name = "name"; 
+        checkbox.value = "value"; 
+        checkbox.id = subject.title; 
+
+        var label = document.createElement('label');
+        label.id = "label";
+        label.innerHTML = subject.title;
+        inportant_container.appendChild(checkbox); 
+        inportant_container.appendChild(label);
+    });
+    var inportant_button = document.createElement('button');
+    inportant_button.innerHTML = "next";
+    inportant_button.id = "next_btn_form";
+    inportant_button.onclick = function(){
+        save_inport_form();
+    };
+    inportant_container.appendChild(inportant_button);
+}
+function save_inport_form(){
+    var inport_form = document.getElementById("inportant_container");
+    //Reference all the CheckBoxes in Table.
+    var checks = inport_form.getElementsByTagName("input");
+    for (var i = 0; i < checks.length; i++) {
+        if (checks[i].checked) {
+            selected_questions.push(checks[i].id);
+        }
+    }
+    //Display the selected CheckBox values.
+    if (selected_questions.length > 0) {
+        console.log(selected_questions);
+    }
+    questions_screen.style.display = "block";
+    inportant_screen.style.display = "none";
+    next_Question();
+}
+
 function next_Question(){
     title_questions.innerHTML = subjects[counter]['title'];
     statement_questions.innerHTML = subjects[counter]['statement'];
@@ -93,6 +137,12 @@ function getPartyPoints(party){
              if (answer[j].statement == subjects[j].parties[i].position){
                  if(party == subjects[j].parties[i].name){
                     points++;
+                    selected_questions.forEach(title=>{
+                        if(subjects[j].title == title){
+                        points++;
+                    }
+                    })
+                    
                  }
                  
              }
